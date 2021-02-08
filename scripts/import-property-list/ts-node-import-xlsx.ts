0cb3defdf57f57cdccd6a443d93ecd88532b2dc4
yeti-detective <yeti.detective@gmail.com>
2021-02-07 20:35:03 -0500

    run 'yarn import-xlsx <relative path to spreadsheet>' to identify sheet, column, and row of cell containing header 'Address' (not case sensitive)

--- scripts/import-property-list/ts-node-import-xlsx.ts
+++ scripts/import-property-list/ts-node-import-xlsx.ts
@@ -1,2 +1,36 @@
 #!/usr/bin/env ts-node-script
-console.log("hello world")
+import fs = require("fs");
+import { XlsxFormatter } from "../../utils/import-xlsx"
+
+const filePath: string = process.argv[2];
+
+if (!filePath) {
+    const usageMsg: string = `
+    Usage: yarn import-xlsx <file path to excel file>
+    `
+    console.log(usageMsg)
+} else {
+    importXlsxPropertyList(filePath)
+}
+
+function importXlsxPropertyList(filePath: string) {
+    let fileBlob: Buffer
+    // first block, obtain input file
+    try {
+        // check for read access to file
+        fs.accessSync(filePath, fs.constants.R_OK)
+        fileBlob = fs.readFileSync(filePath)
+        console.log(`successfully read ${fileBlob.length} bits from file`)
+    } catch (err) {
+        console.warn(err)
+    }
+    
+    // second block, initialize spreadsheet
+    const xlsxFormatter = new XlsxFormatter()
+    try {
+        xlsxFormatter.initSpreadsheet(fileBlob)
+    } catch (err) {
+        console.warn(err)
+    }
+    // third block, parse file to intermediate format
+}
--- utils/import-xlsx.ts
+++ utils/import-xlsx.ts
@@ -5,58 +5,104 @@ type ImportXlsx = {
     read (fileBlob: Buffer): ImportSpreadSheet 
 }
 
-type ImportWorkSheet = {
-    [key: string]: {
-        v: string
+type ImportSpreadSheet = {
+    Sheets: { 
+        [key: string]: ImportWorkSheet
     }
 }
 
-type ImportSpreadSheet = {
-    Sheets: ImportWorkSheet
+type ImportWorkSheet = {
+    [key: string]: ImportCell
 }
 
-export class XlsxImporter {
+type ImportCell = { v: string }
+
+type colRowSheetName = { sheetName: string, col: string, row: string }
+
+type ContentTable = {
+    Address: colRowSheetName,
+
+}
+
+export class XlsxFormatter {
     xlsx: ImportXlsx
     spreadSheet: ImportSpreadSheet
     sheets: string[]
     propertyTable: object
     buildings: IBuildingFields[]
+    contentTable: ContentTable
 
     constructor() {
-       this.xlsx = XLSX;
+       this.xlsx = XLSX
        // initialize properties to empty values
-       this.spreadSheet = { Sheets: {} };
-       this.sheets = [];
-       this.propertyTable = {};
+       this.spreadSheet = { Sheets: {} }
+       this.sheets = []
+       this.propertyTable = {}
        this.buildings = []
     }
 
     initSpreadsheet(fileBlob: Buffer) {
         this.spreadSheet = this.xlsx.read(fileBlob)
         this.sheets = Object.keys(this.spreadSheet.Sheets)
+        this.findContent()
     }
 
-    testMethod(filePath: string): string {
-        return filePath
+    findContent() {
+        let addrColRowSheet: colRowSheetName = { col: '', row: '', sheetName: '' }
+        for (const sheet of this.sheets) {
+            addrColRowSheet = this.findAddressColRow(sheet)
+            if (
+                addrColRowSheet.col.length > 0 &&
+                addrColRowSheet.row.length > 0 &&
+                addrColRowSheet.sheetName.length > 0
+                ) {
+                    break
+                }
+        }
+        console.log(JSON.stringify(addrColRowSheet, undefined, 2))
     }
 
-    // findContent(): string {
-    //     for (const sheet of this.sheets) {
-    //         const addrCol = this.findAddressCol(sheet)
-    //     }
-    //     return ''
-    // }
+    findAddressColRow(sheet: string): colRowSheetName {
+        let sheetName: string = ''
+        let addressCol: string = '' 
+        let addressRow: string = ''
+        const checkSheet = this.spreadSheet.Sheets[sheet]
+        const cellNames: string[] = Object.keys(checkSheet)
+        for (const cellName of cellNames) {
+            if (cellName !== '!ref' && checkSheet[cellName]?.v.toLowerCase() == "address") {
+                sheetName = sheet
+                addressCol = this.getCol(cellName)
+                addressRow = this.getRow(cellName)
+                break
+            }
+        }
+        return { 
+            sheetName, 
+            col: addressCol, 
+            row: addressRow 
+        }
+    }
 
-    // findAddressCol(sheet: string): string {
-    //     // for (const sheet of sheets) {
+    getCol(cell: string): string {
+        // if there is a way to use a RegExp matcher in Typescript, please implement it here
+        return cell.split('').reduce( (a, b): string => {
+            const upperCode: number = b.toUpperCase().charCodeAt(0)
+            if (upperCode >= 65 && upperCode <= 90) {
+                a += b.toUpperCase()
+            }
+            return a
+        }, "")
+    }
 
-    //     // }
-    //     return ''
-    // }
-
-    // getCol(cell: string): string {
-    //     const alpha: RegExp = new RegExp(/[A-Z]/)
-    //     return cell.match(alpha)?[0]
-    // }
+    getRow(cell: string): string {
+        // see above re: String.match()
+        return cell.split('').reduce( (a, b): string => {
+            const code: number = b.charCodeAt(0)
+            if (code >= 48 && code <= 57) {
+                a += b.toUpperCase()
+            }
+            return a
+        }, "")
+    }
 }
 
